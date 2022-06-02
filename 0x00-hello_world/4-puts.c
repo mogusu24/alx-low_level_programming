01acf09cd8db0bea247f339c886a81d543606ceb
beryl mogusu <berylmogusu@gmail.com>
2022-06-02 07:27:46 -0700

    My first alx c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/**
+ * a program that prints exactly Programming is like building a
+ * multilingual puzzlenn
+ */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */ 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
