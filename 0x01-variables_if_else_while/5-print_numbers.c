26a4b22f15443c386b4a820e0e3eac0520e59c36
beryl mogusu <berylmogusu@gmail.com>
2022-06-03 07:01:07 -0700

    Added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		printf("%d", digit);
+		digit++;
+	}
+	printf("\n");
+	return (0);
+}
+
