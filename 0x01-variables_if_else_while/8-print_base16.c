12e020ba2b7cd60de9511a97c203130089ccbfcb
beryl mogusu <berylmogusu@gmail.com>
2022-06-03 07:42:35 -0700

    Added 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:  a program that prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+	char ch;
+	
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+	return (0);
+}
