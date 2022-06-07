10a6005553a5f0ee8314893394370ea355c3f916
beryl mogusu <berylmogusu@gmail.com>
2022-06-07 07:14:12 -0700

    Added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: The number to be checked.
+ *
+ * Return: 1 if positive, 0 if 0 and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
+}
