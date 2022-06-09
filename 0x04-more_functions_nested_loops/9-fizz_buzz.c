9f7a69f12be4b3b7b0d4346065b1eb5febfaeee7
beryl mogusu <berylmogusu@gmail.com>
2022-06-09 14:33:12 -0700

    submitted task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,18 +13,17 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3) == 0)
-			printf("Fizz");
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
 		else if ((num % 5) == 0)
 			printf("Buzz");
-		else if ((num % 3) == 0 && (num % 5) == 0)
-			printf("FizzBuzz");
+		else if ((num % 3) == 0)
+			printf("Fizz");
 		else
 			printf("%d", num);
 
-		if (num == 100)
-			continue;
-		printf(" ");
+		if (num < 100)
+			printf(" ");
 	}
 	printf("\n");
 
