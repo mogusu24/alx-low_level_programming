ffef487d3343488e05e1f1a1a28274718c278103
beryl mogusu <berylmogusu@gmail.com>
2022-06-13 07:18:09 -0700

    Submitted task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,7 +5,7 @@
  */
 void puts_half(char *str)
 {
-	int i = 0, len = 0, n;
+	int i, len = 0, n;
 
 	while (str[len] != '\0')
 		len++;
@@ -13,7 +13,7 @@ void puts_half(char *str)
 	if (len % 2 == 0)
 		n = len / 2;
 	else
-		n = (len - 1) / 2;
+		n = (len + 1) / 2;
 
 	for (i = n; i < len; i++)
 		_putchar(str[i]);
