aabb52cb43597448fe0a28bdb8c8e4bd89aea576
beryl mogusu <berylmogusu@gmail.com>
2022-06-13 06:01:32 -0700

    Submitted task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int i, len = 0, temp;
+
+	while (s[len] != '\0')
+		len++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+
+	}
+}
