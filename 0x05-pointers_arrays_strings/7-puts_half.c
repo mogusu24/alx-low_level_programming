d223129aa02ad15cbb9f021632f1b818946515ca
beryl mogusu <berylmogusu@gmail.com>
2022-06-13 06:58:14 -0700

    Submitted task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string last half to be printed
+ */
+void puts_half(char *str)
+{
+	int i, len = 0, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
