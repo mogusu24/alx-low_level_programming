6bf5bf15161f0d8eb2ac0e256c90b250c0f34694
beryl mogusu <berylmogusu@gmail.com>
2022-06-13 11:56:51 -0700

    Submitted task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,10 +12,10 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	i = 0;
-	while (src[i])
+	while (i >= 0)
 	{
-		dest[i] = src[i];
-		if (src[i] == '\0')
+		*(dest + i) = *(src + i);
+		if (*(src + i) == '\0')
 			break;
 		i++;
 	}
