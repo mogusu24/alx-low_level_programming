39487de12515b3b6a24f11b562a62c724f552bba
beryl mogusu <berylmogusu@gmail.com>
2022-06-16 04:05:31 -0700

    Submitted task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,19 +8,23 @@
  */
 char *leet(char *str)
 {
-	int index1 = 0, index2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	/* conversion tables */
+	char letters[] = "aeotl";
+	char values[] = "43071";
 
-	while (str[index1])
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (index2 = 0; index2 <= 7; index2++)
+		for (j = 0; letters[j] != '\0'; j++)
 		{
-			if (str[index1] == leet[index2] ||
-			    str[index1] - 32 == leet[index2])
-				str[index1] = index2 + '0';
+			if (str[i] == letters[j] || str[i] == letters[j] - 32)
+			{
+				str[i] = values[j];
+				break;
+			}
 		}
-
-		index1++;
 	}
 
 	return (str);
