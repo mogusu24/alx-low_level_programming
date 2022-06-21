8819825a49b36bc5ef4d7e39136391d6ed9a55bb
beryl mogusu <berylmogusu@gmail.com>
2022-06-21 02:28:54 -0700

    Submitted task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr -  locates a character in a string
@@ -16,6 +17,9 @@ char *_strchr(char *s, char c)
 		if (c == s[i])
 			return (s + i);
 	}
+
+	if (c == s[i])
+		return (s + i);
 
 	return (NULL);
 }
