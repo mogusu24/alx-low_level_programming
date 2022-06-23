d61111a6f778d99a2d28847840c169f73f62bd6f
beryl mogusu <berylmogusu@gmail.com>
2022-06-23 02:17:40 -0700

    Submitted task 8

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,3 +11,7 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int find_strlen(char *s);
 int check_palindrome(char *s, int len, int index);
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
